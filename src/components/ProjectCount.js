0ce749ccce418d6972802c3d0a7011459dd784b1
RahulRj09 <rahul18@navgurukul.org>
2020-07-25 16:05:21 +0530

    Add. project count

--- src/components/ProjectCount.js
+++ src/components/ProjectCount.js
@@ -3,7 +3,6 @@ import { connect } from 'react-redux'
 import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardHeader from '@material-ui/core/CardHeader';
-import CardMedia from '@material-ui/core/CardMedia';
 import CardContent from '@material-ui/core/CardContent';
 import Avatar from '@material-ui/core/Avatar';
 import Typography from '@material-ui/core/Typography';
